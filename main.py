b8771fd8ac097f7e8b3d94bf635bbc8fbaeb497e
user <user@mail.com>
2025-10-16 09:43:27 +0100

    Add function to count words in text

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ def get_book_text(filepath):
         file_contents = file.read()
     return file_contents
 
+def count_words(text):
+    counter = 0
+    # split by break lines
+    lines = text.split('\n') 
+    for line in lines:
+        words = line.split(' ')
+        # print(words)
+        for word in words:
+            if len(word) > 0:
+                counter += 1
+    return counter
+    
 def main():
     filepath = "./books/frankenstein.txt"
-    print(get_book_text(filepath))
-
+    text = get_book_text(filepath)
+    print(f"Found {count_words(text)} total words")
 main()
 
