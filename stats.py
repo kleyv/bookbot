011c56f9875b9d6ab982bb393ed6d7a8afcf5c94
user <user@mail.com>
2025-10-16 17:38:36 +0100

    Refactor count_words()

--- stats.py
+++ stats.py
@@ -1,12 +1,6 @@
 def count_words(text):
-    counter = 0
-    lines = text.split('\n') 
-    for line in lines:
-        words = line.split(' ')
-        for word in words:
-            if len(word) > 0:
-                counter += 1
-    return counter
+    words = text.split() 
+    return len(words)
 
 def sort_on(items):
     return items["count"]
